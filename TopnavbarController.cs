9a7dcc53e873a7f7a2e10285b9ac3a66c1f038f8
Đình Quý <93045247+dinhquy23@users.noreply.github.com>
2022-03-11 20:11:26 +0700

    Add files via upload

---
+++ TopnavbarController.cs
@@ -0,0 +1,20 @@
+﻿using Glass.Mapper.Sc.Web.Mvc;
+using System.Web.Mvc;
+using MockProject.Foundation.Mvc.Controllers;
+using MockProject.Feature.Sample.Models.TemplateModels;
+
+namespace MockProject.Feature.Sample.Controller
+{
+    public class TopnavbarController : BaseController
+    {
+        public TopnavbarController(IMvcContext mvcContext) : base(mvcContext)
+        {
+        }
+
+        public ActionResult GetNavigation()
+        {
+            var model = MvcContext.GetDataSourceItem<TopnavbarModel>();
+            return View("~/Views/MockProject/Header/Navigation.cshtml", model);
+        }
+    }
+}
